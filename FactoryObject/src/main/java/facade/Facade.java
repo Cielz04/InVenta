d7b067c64ab75a75d1c403d2faa2b4aaf1f45d3c
JonathanAstorga <jonathan.astorga245791@potros.itson.edu.mx>
2025-04-25 22:13:18 -0700

    subsistema inventario

--- FactoryObject/src/main/java/facade/Facade.java
+++ FactoryObject/src/main/java/facade/Facade.java
@@ -6,20 +6,42 @@ package facade;
 
 import factory.AbstractDAOFactory;
 import factory.DAOFactory;
+import java.util.List;
 import org.itson.diseniosofware.mifarmaciagi.persistencia.Conexion.Conexion;
 import org.itson.diseniosofware.mifarmaciagi.persistencia.Conexion.IConexion;
+import org.itson.diseniosofware.mifarmaciagi.persistencia.daos.ProductosDAO;
+import org.itson.diseniosofware.mifarmaciagi.persistencia.entidades.Producto;
 
 /**
  *
  * @author jl4ma
  */
-public class Facade implements IFacade{
-    
+public class Facade implements IFacade {
+
     private IConexion conexion;
     private AbstractDAOFactory fabrica;
-    
+
     public Facade() {
         conexion = new Conexion();
         fabrica = new DAOFactory(conexion);
+    }
+
+    public List<Producto> buscarProductos() throws Exception {
+
+        try {
+            List<Producto> lista = fabrica.getProductosDAO().findAll();
+            if(lista.isEmpty()){
+                throw new Exception("La lista esta vacia");
+            }
+            return lista;
+        } catch (Exception e) {
+            System.out.println(e);
+            throw new Exception(e);
+        }
+    }
+    
+    public Producto agregarProducto(Producto producto){
+        fabrica.getProductosDAO().save(producto);
+        return fabrica.getProductosDAO().findByCodigo(producto.getCodigo());
     }
 }
---
+++ FarmaciaGI_SubsistemaGestorInventario/src/main/java/BO/GestorInvetario.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BO;
+
+import conversores.ConversorProducto;
+import interfaces.IGestorInventario;
+
+/**
+ *
+ * @author Dell
+ */
+public class GestorInvetario implements IGestorInventario{
+    private ConversorProducto conversorProducto = new ConversorProducto();
+    
+    public GestorInvetario() {
+    }
+    
+    
+    
+}
